a66fdeb93cb207a96536df3b6d88fe67e9312643
Gerardo Belot <gbelot2003@hotmail.com>
2022-02-27 01:19:31 -0600

    :lipstick:

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -4,7 +4,10 @@
 <div class="row justify-content-center">
     <div class="col-md-12">
         <div class="card">
-            <div class="card-header">{{ $thread->title }}</div>
+            <div class="card-header">
+                <a href="#">{{ $thread->creator->name }}</a> posted:
+                <strong>{{ $thread->title }}</strong>
+            </div>
             <div class="card-body">
                  <article>
                     <div class="body">{{ $thread->body }}</div>
