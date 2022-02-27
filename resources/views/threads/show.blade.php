87700ceef09db7adc9b8fed42056a81d48b7284d
Gerardo Belot <gbelot2003@hotmail.com>
2022-02-27 00:57:50 -0600

    :rocket: agregando stubs

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -14,19 +14,7 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="card">
-            @foreach ($thread->replies as $reply)
-                <div class="card-header">
-                    <strong><a href="#">{{ $reply->owner->name }}</a> said</strong>
-                     {{ $reply->created_at->diffForHumans() }}</div>
-                <div class="card-body">
-
-                    {{ $reply->body }}
-                </div>
-                <hr />
-            @endforeach
-
-        </div>
+        @include('threads._replies')
     </div>
 </div>
 
